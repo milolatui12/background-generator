08770edc520f39f6399e8b4f1ac78720806832fe
milo <tranmilo66@gmail.com>
2020-03-03 09:26:43 +0700

    starting new file

---
+++ script.js
@@ -0,0 +1,14 @@
+let body = document.getElementById("body");
+let color1 = document.querySelector("#color1");
+let color2 = document.querySelector("#color2");
+let h3 = document.querySelector("h3");
+
+
+
+function changeColor() {
+    body.style.background = "linear-gradient(to right, " + color1.value + ", " +color2.value + ")";
+    h3.textContent = body.style.background;
+}
+
+color1.addEventListener("input", changeColor);
+color2.addEventListener("input", changeColor);
